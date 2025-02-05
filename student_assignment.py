a410811afcedcec1f0d76cd819937ee267415f1e
noctis_li <noctis_li@asus.com>
2025-02-05 17:33:38 +0800

    hw1 2025-02-05
    Signed-off-by: noctis_li <noctis_li@asus.com>

--- student_assignment.py
+++ student_assignment.py
@@ -1,3 +1,5 @@
+import os
+
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
@@ -7,7 +9,13 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    q1_pdf_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), q1_pdf)
+    print(q1_pdf_path)
+    loader = PyPDFLoader(file_path=q1_pdf_path)
+    docs = loader.load()
+    text_splitter = CharacterTextSplitter()
+    chunk = text_splitter.split_documents(docs)
+    print(chunk[-1])
 
 def hw02_2(q2_pdf):
-    pass
+    q2_pdf_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), q2_pdf)
